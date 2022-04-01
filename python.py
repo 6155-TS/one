20105f5c6c1ea4cbe7e1c6daaa0eed8c1789303b
betul <betulkaplanduran@gmail.com>
2022-04-01 11:57:33 +0300

    v1

---
+++ python.py
@@ -0,0 +1,9 @@
+
+nt_back(word):
+      if len(word)<=1:
+              return word
+            a= word[1:len(word)-1]
+              b=word[len(word)-1]
+                c=word[0]
+                  return b+a+c
+              front_back("clarusway")
